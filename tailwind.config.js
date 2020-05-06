3e8ae0d202984b0a590d8b9901314c9e7da3410c
Pascal Andy <pascal@firepress.org>
2020-05-05 23:54:07 -0400

    update from crock

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,16 +2,13 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   purge: ["./src/**/*.js"],
-  variants: {},
   theme: {
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
-    },
+    }
   },
-  plugins: [
-    require('@tailwindcss/ui'),
-  ]
+  variants: {},
+  plugins: [require('@tailwindcss/ui')]
 }
-
